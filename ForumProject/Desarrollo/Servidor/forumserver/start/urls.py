ccc7630d4eea3cb1e82a4bc051e7e3df58f72e96
JoseSantos <jose.santos2@unmsm.edu.pe>
2019-05-07 19:44:27 -0500

    Añadidos serializers para manejar las llamadas de las API's

--- ForumProject/Desarrollo/Servidor/forumserver/start/urls.py
+++ ForumProject/Desarrollo/Servidor/forumserver/start/urls.py
@@ -1,12 +1,13 @@
 """ URLs for gestion app"""
 from django.conf import settings
-from django.conf.urls import url
 from django.conf.urls.static import static
+from django.urls import path
+
 from start.views import RegisterUserAPI, AuthenticateUserAPI
 
 app_name = 'start'
 
 urlpatterns = [
-    url('register-api/', RegisterUserAPI.as_view()),
-    url('login-api/', AuthenticateUserAPI.as_view()),
+    path('register-api/', RegisterUserAPI.as_view()),
+    path('login-api/', AuthenticateUserAPI.as_view()),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
