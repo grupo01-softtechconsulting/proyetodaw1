35cbcbb86fe457578f78109ea368bd868207814b
jean enriquez <jean.enriquez@unmsm.edu.pe>
2019-07-04 08:34:50 -0500

    Añadido snackbar component

---
+++ ForumProject/Desarrollo/Cliente/forumclient/src/components/Snackbar.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-snackbar
+    v-model="snackbarOptions.show"
+    :bottom="y === 'bottom'"
+    :left="x === 'left'"
+    :multi-line="mode === 'multi-line'"
+    :right="x === 'right'"
+    :timeout="timeout"
+    :top="y === 'top'"
+    :vertical="mode === 'vertical'"
+  >
+    {{ snackbarOptions.text }}
+    <v-btn color="primary" flat @click="snackbarOptions.show = false">Cerrar</v-btn>
+  </v-snackbar>
+</template>
+
+<script>
+export default {
+  props: {
+    snackbarOptions: {
+      show: false,
+      text: ''
+    }
+  },
+
+  data: () => ({
+    //snackbar: false,
+    y: "top",
+    x: null,
+    mode: "",
+    timeout: 3000,
+    //text: "Hello, I'm a snackbar"
+  })
+};
+</script>
